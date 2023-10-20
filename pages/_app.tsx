05f99ccc6baa131c29c510e6df3d8fd1286216bf
AsaMitaka <cepelits@gmail.com>
2023-10-20 20:06:03 +0300

    app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
-import Layout from '@/components/ui/layout';
-import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
+import '@/styles/globals.css';
+
+import LoginModal from '@/components/modal/loginmodal';
+import RegistrationModal from '@/components/modal/registrationmodal';
+import Layout from '@/components/ui/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <LoginModal />
+      <RegistrationModal />
       <Toaster />
       <Layout>
         <Component {...pageProps} />
