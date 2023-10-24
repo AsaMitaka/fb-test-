7c9c4824b189bddaf924a5bdc844410220d0aea8
AsaMitaka <cepelits@gmail.com>
2023-10-24 23:06:45 +0300

    edit modal

--- components/modal/editmodal.tsx
+++ components/modal/editmodal.tsx
@@ -1,15 +1,39 @@
 import useEditUserModal from '@/hooks/useEditModal';
 import Modal from './modal';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
+import Input from '../ui/input';
+import useCurrent from '@/hooks/useCurrent';
+import useUser from '@/hooks/useUser';
+import UploadImage from '../ui/uploadimage';
+import axios from 'axios';
 
 const EditUserModal = () => {
+  const { data: currentUser } = useCurrent();
+  const { mutate: mutateFetchUser } = useUser(currentUser?.id);
   const useEditUser = useEditUserModal();
+
+  const [nickname, setNickname] = useState('');
+  const [bio, setBio] = useState('');
+  const [profileImage, setProfileImage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = useCallback(() => {
+  useEffect(() => {
+    setBio(currentUser?.bio);
+    setNickname(currentUser?.nickname);
+    setProfileImage(currentUser?.profileImage);
+  }, [currentUser?.bio, currentUser?.profileImage, currentUser?.nickname]);
+
+  const handleSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
+
+      await axios.patch('/api/edit', {
+        bio,
+        nickname,
+        profileImage,
+      });
+      mutateFetchUser();
 
       toast.success('Success');
       useEditUser.onClose();
@@ -19,9 +43,32 @@ const EditUserModal = () => {
     } finally {
       setIsLoading(false);
     }
-  }, [setIsLoading, useEditUser]);
+  }, [bio, mutateFetchUser, nickname, profileImage, setIsLoading, useEditUser]);
 
-  const bodyContent = <></>;
+  const bodyContent = (
+    <div className="mt-4 flex flex-col gap-3">
+      <Input
+        disabled={isLoading}
+        onChange={(e) => setNickname(e.target.value)}
+        placeholder="Nickname"
+        type="text"
+        value={nickname}
+      />
+      <Input
+        disabled={isLoading}
+        onChange={(e) => setBio(e.target.value)}
+        placeholder="Bio"
+        type="text"
+        value={bio || ''}
+      />
+      <UploadImage
+        disabled={isLoading}
+        label="Profile Image"
+        onChange={(image) => setProfileImage(image)}
+        value={profileImage}
+      />
+    </div>
+  );
 
   return (
     <Modal
