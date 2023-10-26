eee087832a3e33f6a21cca1a26c0f7a645b96de9
AsaMitaka <cepelits@gmail.com>
2023-10-26 21:08:06 +0300

    userbio minor fix

--- components/user/userbio.tsx
+++ components/user/userbio.tsx
@@ -3,6 +3,9 @@ import useUser from '@/hooks/useUser';
 import Avatar from './avatar';
 import Button from '../ui/button';
 import useEditUserModal from '@/hooks/useEditModal';
+import { useCallback } from 'react';
+import useFollow from '@/hooks/useFollow';
+import useLoginModal from '@/hooks/useLoginModal';
 
 interface UserBioProps {
   userId: string;
@@ -11,7 +14,20 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser } = useCurrent();
   const { data: fetchedCurrentUser } = useUser(userId as string);
+  const { isFollowing, toggleFollow } = useFollow(userId);
+  console.log(isFollowing);
+
   const useEditUser = useEditUserModal();
+  const loginModal = useLoginModal();
+
+  const handleFollow = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    toggleFollow();
+  }, [currentUser, loginModal, toggleFollow]);
+  console.log(fetchedCurrentUser);
 
   return (
     <div className="border-b-[1px] border-black p-2 pb-4">
@@ -19,10 +35,15 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         <div className="flex flex-row gap-3 items-center justify-between p-2">
           <Avatar userId={userId} isLarge hasBorder />
           <div className="flex justify-end p-2">
-            {currentUser.id === userId ? (
+            {currentUser?.id === userId ? (
               <Button label="Edit" onClick={useEditUser.onOpen} />
             ) : (
-              <Button label={'Follow'} onClick={() => {}} />
+              <Button
+                label={isFollowing ? 'Unfollow' : 'Follow'}
+                onClick={handleFollow}
+                outline={isFollowing}
+                secondary={!isFollowing}
+              />
             )}
           </div>
         </div>
@@ -33,15 +54,15 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
           <div className="mt-1 flex flex-row items-center gap-5">
             <div className="flex items-center gap-1">
               <p className="text-black cursor-pointer hover:underline">
-                {fetchedCurrentUser?.followingIds.length}
-              </p>
-              <p className="text-neutral-500">Following</p>
-            </div>
-            <div className="flex items-center gap-1">
-              <p className="text-black cursor-pointer hover:underline">
                 {fetchedCurrentUser?.followersCount || 0}
               </p>
               <p className="text-neutral-500">Followers</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <p className="text-black cursor-pointer hover:underline">
+                {fetchedCurrentUser?.followingIds?.length}
+              </p>
+              <p className="text-neutral-500">Following</p>
             </div>
           </div>
         </div>
